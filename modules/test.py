cdffc6876f640a1a3ed4db39af08fdafa20c322b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-12 18:00:24 +0400

    Test module's config fix

--- modules/test.py
+++ modules/test.py
@@ -1,40 +1,38 @@
-module_config = [
-    {
-        "name": "test",
-        "public_name": {
-            "en": "Test",
-            "ru": "Тест"
-        },
-        "description": {
-            "en": "Check bot for basic operability",
-            "ru": "Проверка бота на общую работоспособность"
-        },
-        "regexps": {
-                "en": [
-                    "!t(est)?"
-                ],
-                "ru": [
-                    "!т(ест)?"
-                ]
-        },
-        "command_format": {
-            "en": "!test",
-            "ru": "!тест"
-        },
-        "examples": {
+module_config ={
+    "name": "test",
+    "public_name": {
+        "en": "Test",
+        "ru": "Тест"
+    },
+    "description": {
+        "en": "Check bot for basic operability",
+        "ru": "Проверка бота на общую работоспособность"
+    },
+    "regexps": {
             "en": [
-                "!test",
-                "!t"
+                "!t(est)?"
             ],
             "ru": [
-                "!тест",
-                "!т"
+                "!т(ест)?"
             ]
-        },
-        "adapters": [],
-        "options": {}
-    }
-]
+    },
+    "command_format": {
+        "en": "!test",
+        "ru": "!тест"
+    },
+    "examples": {
+        "en": [
+            "!test",
+            "!t"
+        ],
+        "ru": [
+            "!тест",
+            "!т"
+        ]
+    },
+    "adapters": [],
+    "options": {}
+}
 
 answer = {
     "en": "Test completed. Message: '{text}' from {name}",
