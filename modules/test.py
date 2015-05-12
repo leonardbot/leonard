7e0f4e2b7c156da8953b27b86fcb014370569593
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-12 11:21:19 +0400

    Moved module config to module file

--- modules/test.py
+++ modules/test.py
@@ -1,5 +1,45 @@
 from sheldon import send_message
 
+module_config = [
+    {
+        "name": "test",
+        "public_name": {
+            "en": "Test",
+            "ru": "Тест"
+        },
+        "description": {
+            "en": "Check bot for basic operability",
+            "ru": "Проверка бота на общую работоспособность"
+        },
+        "regexps": {
+                "en": [
+                    "!t(est)?"
+                ],
+                "ru": [
+                    "!т(ест)?"
+                ]
+        },
+        "command_format": {
+            "en": "!test",
+            "ru": "!тест"
+        },
+        "examples": {
+            "en": [
+                "!test",
+                "!t"
+            ],
+            "ru": [
+                "!тест",
+                "!т"
+            ]
+        },
+        "adapters": [],
+        "options": {}
+    }
+]
+
+
+
 def get_answer(message):
     send_message(
         message_text="Тест пройден. Cообщение: '{text}' от {name}".format(
