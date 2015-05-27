bf02057ea791377a5a920091f271a00567559f97
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-27 12:42:27 +0400

    Yet another fix for tests for site module

--- tests/test_site_module.py
+++ tests/test_site_module.py
@@ -20,25 +20,25 @@ def test_module_in_loaded_modules():
 
 def test_getting_answer_without_protocol():
     bot.parse_message(message)
-    assert bot.sent_messages[-1]['message_text'] == "Site working."
+    assert bot.sent_messages[-1]['message_text'] == "Site working"
 
 
 def test_getting_answer_with_protocol():
     message['text'] = '!site http://yandex.ru'
     bot.parse_message(message)
-    assert bot.sent_messages[-1]['message_text'] == "Site working."
+    assert bot.sent_messages[-1]['message_text'] == "Site working"
 
 
 def test_getting_answer_with_not_working_site():
     message['text'] = '!site http://yandedsrsasfrex.ru'
     bot.parse_message(message)
-    assert bot.sent_messages[-1]['message_text'] == "Site not working."
+    assert bot.sent_messages[-1]['message_text'] == "Site not working"
 
 
 def test_getting_answer_with_incorrect_site():
     message['text'] = '!site lulz'
     bot.parse_message(message)
-    assert bot.sent_messages[-1]['message_text'] == "Site not working."
+    assert bot.sent_messages[-1]['message_text'] == "Site not working"
 
 
 def test_getting_answer_without_parameter():
