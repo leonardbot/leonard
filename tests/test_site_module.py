cdf9bf0abd67169498304eb36e5c00c38150603e
Seva Zhidkov <zhidkovseva@gmail.com>
2015-05-27 12:39:36 +0400

    Fix tests for site module

--- tests/test_site_module.py
+++ tests/test_site_module.py
@@ -38,7 +38,7 @@ def test_getting_answer_with_not_working_site():
 def test_getting_answer_with_incorrect_site():
     message['text'] = '!site lulz'
     bot.parse_message(message)
-    assert bot.sent_messages[-1]['message_text'] == "Incorrect site"
+    assert bot.sent_messages[-1]['message_text'] == "Site not working."
 
 
 def test_getting_answer_without_parameter():
